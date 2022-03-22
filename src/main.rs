fd962374d135bab441aeef3130ef5a67fc5ef3b5
Kodai Kabasawa <kabaaa1126@gmail.com>
2022-03-22 17:00:55 +0900

    test: base of search function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
     println!("Hello, world!");
 }
+
+pub fn search<'a>(query: &'a str) -> Vec<&'a str> {
+    let mut v = Vec::new();
+    v.push(query);
+    v
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn return_emoji() {
+        let query = "face";
+        assert_eq!([query].to_vec(), search(query))
+    }
+}
