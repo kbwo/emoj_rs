995558fe1dc3646887bcc7a5827906aa18e1eba1
Kodai Kabasawa <kabaaa1126@gmail.com>
2022-03-23 13:45:33 +0900

    add function to limit numbers of string array

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,15 @@ fn main() {
     println!("Hello, world!");
 }
 
+/// max numbers of emojis are 7
+pub fn limit_emojis(searched_emojis: &Vec<String>) -> &[String] {
+    if searched_emojis.len() > 7 {
+        &searched_emojis[0..7]
+    } else {
+        &searched_emojis[..]
+    }
+}
+
 pub fn read_json() -> Result<Value, Box<(dyn std::error::Error)>> {
     let mut emoji_file = File::open("node_modules/emojilib/dist/emoji-en-US.json")?;
     let mut emoji_file_str = String::new();
@@ -16,13 +25,13 @@ pub fn read_json() -> Result<Value, Box<(dyn std::error::Error)>> {
 }
 
 pub fn search(query: &str) -> Result<Vec<String>, Box<(dyn std::error::Error)>> {
-    let re1 = Regex::new(r"\s").unwrap();
-    let re2 = Regex::new(r"\W").unwrap();
+    let reg_empty = Regex::new(r"\s").unwrap();
+    let reg_not_word = Regex::new(r"\W").unwrap();
     let lowercase = query.to_lowercase();
-    let regex_source = re1
+    let regex_source = reg_empty
         .split(&lowercase)
         .map(|x| {
-            re2.replace(x, "");
+            reg_not_word.replace(x, "");
             x
         })
         .filter(|x| x.len() > 0)
@@ -52,7 +61,23 @@ mod test {
     use super::*;
 
     #[test]
-    fn return_emoji() {
+    fn limit_numbers_of_str() {
+        let test_strings = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+            .map(|x| x.to_string())
+            .to_vec();
+        let limited_strings = limit_emojis(&test_strings);
+        assert_eq!(7, limited_strings.len());
+    }
+
+    #[test]
+    fn numbers_of_tiny_str() {
+        let test_strings = ["a", "b", "c", "d"].map(|x| x.to_string()).to_vec();
+        let limited_strings = limit_emojis(&test_strings);
+        assert_eq!(4, limited_strings.len());
+    }
+
+    #[test]
+    fn search_emoji() {
         let query = "face";
         match search(query) {
             Ok(result) => {
